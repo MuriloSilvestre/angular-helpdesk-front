fa47ce01f06700e0a2765d2e2f7c326591672c39
Murilo Silvestre <m.serratosilvestre@gmail.com>
2023-03-07 21:22:19 -0300

    Inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NavComponent } from './components/nav/nav.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NavComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
